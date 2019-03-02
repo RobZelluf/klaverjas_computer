dc795658d0e34ca5fc8a5c223a23eeaabe2ba495
RobZelluf <robzelluf@hotmail.com>
2019-03-02 16:49:40 +0100

    Finish turn taking and deciding winner

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import numpy as np
 import random
-from rules import verify_play
+from rules import *
 
-values = ["7", "8", "9", "B", "V", "K", "10", "A"]
+values = ["7", "8", "9", "B", "V", "H", "10", "A"]
 suits = ["K", "S", "H", "R"]
 
 
@@ -33,10 +33,24 @@ def get_order(firstTurn):
 
 def get_trump():
     trump = input("Choose trump:")
-    if trump in suits:
+    if trump in [suit.lower() for suit in suits]:
         return trump
     else:
         return get_trump()
+
+
+def get_card(num_cards):
+    index = input("Card")
+    try:
+        index = int(index)
+        if index in list(range(num_cards)):
+            return int(index)
+        else:
+            print("Input is not a number between 0 and", num_cards)
+            return get_card(num_cards)
+    except:
+        print("Input is not a number between 0 and", num_cards)
+        return get_card(num_cards)
 
 
 class Game:
@@ -47,6 +61,20 @@ class Game:
             self.players.append(Player(i))
 
         self.currentPlayersTurn = 0
+
+        self.team1_points = 0
+        self.team2_points = 0
+
+    def add_points(self, player, points):
+        if player in [0, 2]:
+            self.team1_points += points
+        else:
+            self.team2_points += points
+
+    def next_turn(self):
+        self.currentPlayersTurn += 1
+        if self.currentPlayersTurn == 4:
+            self.currentPlayersTurn = 0
 
 
 class Player:
@@ -74,7 +102,7 @@ class Round:
 
 
 class Hand:
-    def __init__(self):
+    def __init__(self, starting_player):
         self.trump = None
 
         # Shuffle and divide cards
@@ -95,15 +123,29 @@ class Hand:
         self.current_round_number = 1
         self.rounds_played = []
 
+        self.current_round = None
+        self.currentPlayersTurn = starting_player
+
+        self.team1_points = 0
+        self.team2_points = 0
+
+    def add_points(self, player, points):
+        if player in [0, 2]:
+            self.team1_points += points
+        else:
+            self.team2_points += points
+
     def set_trump(self, trump):
         self.trump = trump
 
     def start_round(self, starting_player):
         self.current_round = Round(starting_player)
+        self.currentPlayersTurn = starting_player
 
     def play_card(self, player, card):
         if verify_play(card, self.players[player].cards, player, self.current_round):
             self.current_round.cards_played[player] = card
+            self.players[player].cards.remove(card)
             return True
         else:
             return False
@@ -126,28 +168,30 @@ class Hand:
 def main():
     game = Game()
     while game.hand <= 16:
-        hand = Hand()
-        hand.start_round(game.currentPlayersTurn)
+        hand = Hand(game.currentPlayersTurn)
         hand.print_cards(game.currentPlayersTurn)
-        trump = get_trump()
+        trump = get_trump().capitalize()
         for i in range(8):
-            order = get_order(game.currentPlayersTurn)
+            hand.start_round(hand.currentPlayersTurn)
+            order = get_order(hand.currentPlayersTurn)
             for player in order:
+                print("\nPlayer:", player, "- Trump:", map_suit(trump))
                 hand.print_table()
                 hand.print_cards(player)
                 while True:
-                    played_card = int(input("card"))
+                    played_card = get_card(len(hand.players[player].cards))
                     played_card = hand.players[player].cards[played_card]
                     if hand.play_card(player, played_card):
                         break
                     else:
                         print("Invalid move!")
 
-            # Decide winner
-            # Reset hand
+            winner = get_winner(hand.current_round.cards_played, trump, order[0])
+            points = get_total_points(hand.current_round.cards_played, trump)
 
-            # play game
-            pass
+            hand.add_points(winner, points)
+            hand.currentPlayersTurn = winner
+            print(hand.team1_points, hand.team2_points)
 
 
 main()
--- rules.py
+++ rules.py
@@ -1,2 +1,64 @@
+import numpy as np
+
+trump_order = ["B", "9", "A", "10", "H", "V", "8", "7"]
+normal_order = ["A", "10", "H", "V", "B", "9", "8", "7"]
+
+trump_points = {"B": 20,
+                "9": 14,
+                "A": 11,
+                "10": 10,
+                "H": 4,
+                "V": 3,
+                "8": 0,
+                "7": 0}
+
+normal_points = {"A": 11,
+                 "10": 10,
+                 "H": 4,
+                 "V": 3,
+                 "B": 2,
+                 "9": 0,
+                 "8": 0,
+                 "7": 0}
+
+
+def get_card_points(card, trump):
+    if trump:
+        return trump_points[card.value]
+    else:
+        return normal_points[card.value]
+
+
+def get_total_points(cards, trump):
+    total = 0
+    for card in cards:
+        total += get_card_points(card, card.suit == trump)
+
+    return total
+
+
+def get_highest_card(cards, trump):
+    if trump:
+        best_index = min([trump_order.index(card.value) for card in cards])
+        best_card = [card for card in cards if trump_order.index(card.value) == best_index][0]
+    else:
+        best_index = min([normal_order.index(card.value) for card in cards])
+        best_card = [card for card in cards if normal_order.index(card.value) == best_index][0]
+
+    return best_card
+
+
 def verify_play(card, hand, player, round):
     return True
+
+
+def get_winner(round, trump, starting_player):
+    leading_color = round[starting_player].suit
+    if trump in [card.suit for card in round]:
+        highest_card = get_highest_card([card for card in round if card.suit == trump], True)
+    else:
+        highest_card = get_highest_card([card for card in round if card.suit == leading_color], False)
+
+    winner = round.index(highest_card)
+
+    return winner
